edd0f713ff693c46e444bcb34a1c6a3b49a06767
nirinja <demsarnika@gmail.com>
2024-11-24 02:47:29 +0100

    api base

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -33,15 +37,19 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.support.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/app/src/main/java/com/example/receptiapp/MainActivity2.kt
+++ app/app/src/main/java/com/example/receptiapp/MainActivity2.kt
@@ -5,20 +5,64 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.receptiapp.databinding.ActivityMain2Binding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    private var iskanoText: String? = null
+    private var sestavineText: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val iskanoText = intent.getStringExtra("iskano")
-        val sestavineText = intent.getStringExtra("sestavine")
+        iskanoText = intent.getStringExtra("iskano") ?: "Tamiya"
+        sestavineText = intent.getStringExtra("sestavine") ?: ""
+        fetchRecipesData()
+    }
 
+    private fun fetchRecipesData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val url = URL("https://www.themealdb.com/api/json/v1/1/search.php?s=$iskanoText")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.connect()
+                if (connection.responseCode == 200) {
+                    val inputStream = connection.inputStream
+                    val response = inputStream.bufferedReader().use { it.readText() }
+                    val jsonObject = JSONObject(response)
+                    val recipesArray = jsonObject.getJSONArray("meals")
+                    val recipe = recipesArray.getJSONObject(0)
+                    val strInstructions = recipe.getString("strInstructions")
+                    val strMeal = recipe.getString("strMeal")
+
+                    // Posodobi UI na glavni niti
+                    CoroutineScope(Dispatchers.Main).launch {
+                        binding.imeJedi.text = strMeal
+                        binding.navodila.text = strInstructions
+                    }
+                }
+            } catch (e: SecurityException) {
+                println("Security Exception: ${e.message}")
+            } catch (e: Exception) {
+                println("General Exception: ${e.message}")
+                e.printStackTrace()
+            }
+        }
     }
 }
